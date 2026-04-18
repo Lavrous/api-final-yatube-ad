fd59441f305a436b6e38195f81b7e853bc051d90
Евгений Коваленко <zenakovalenko1@gmail.com>
2026-04-18 12:19:01 +0700

    yandex stuff

--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -4,17 +4,6 @@ from django.db import models
 User = get_user_model()
 
 
-class Post(models.Model):
-    text = models.TextField()
-    pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
-    author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='posts')
-    image = models.ImageField(
-        upload_to='posts/', null=True, blank=True)
-
-    def __str__(self):
-        return self.text
-
 
 class Group(models.Model):
     title = models.CharField(max_length=200)
@@ -24,6 +13,23 @@ class Group(models.Model):
     def __str__(self):
         return self.title
 
+class Post(models.Model):
+    text = models.TextField()
+    pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='posts')
+    image = models.ImageField(
+        upload_to='posts/', null=True, blank=True)
+    group = models.ForeignKey(
+        Group,
+        on_delete=models.SET_NULL,
+        related_name='posts',
+        blank=True,
+        null=True
+    )
+
+    def __str__(self):
+        return self.text
 
 class Comment(models.Model):
     author = models.ForeignKey(
@@ -37,17 +43,17 @@ class Comment(models.Model):
         db_index=True
     )
 
-    class Follow(models.Model):
-        user = models.ForeignKey(
-            User,
-            on_delete=models.CASCADE,
-            related_name='follower'
-        )
-        following = models.ForeignKey(
-            User,
-            on_delete=models.CASCADE,
-            related_name='following'
-        )
+class Follow(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='follower'
+    )
+    following = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='following'
+    )
 
     class Meta:
         constraints = [
